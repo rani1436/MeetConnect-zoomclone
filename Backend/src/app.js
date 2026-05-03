7de83f2c7b9516a5810d02b462a79d4195b03081
Rani Gupta <ranigupta6236@gmail.com>
2026-05-03 11:52:27 +0530

    Ready for Deployment

---
+++ Backend/src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+const start = async () => {
+    app.set("mongo_user");
+    const connectionDb = await mongoose.connect("mongodb+srv://ranigupta1436:14LDohYmjjMGrG2i@zoomcluster.n5skkk9.mongodb.net/?appName=ZoomCluster")
+
+
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENING ON PORT 8000")
+    });
+
+
+
+}
+
+
+
+start();
